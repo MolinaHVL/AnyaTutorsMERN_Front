f5d02a556369221b7d0c4619cc5d344ee4c8df3f
MolinaHVL <christopher.molina@cetys.edu.mx>
2023-03-13 17:25:26 -0700

    error fixed images

--- anya_front/src/pages/index/index.jsx
+++ anya_front/src/pages/index/index.jsx
@@ -1,10 +1,14 @@
 ﻿import React, { useState, useEffect } from 'react';
 import './styl.css';
 import { useNavigate } from "react-router-dom"
+import { Box } from '@mui/system';
+import luna from "../../fotos/Moon2.png";
+import mountainFront from "../../fotos/mountains_front.png";
+import mountainBack from "../../fotos/mountains_behind.png";
+import stars from "../../fotos/stars.png";
 
 
-
-function Index() {
+const Index = () => {
     const [scrollValue, setScrollValue] = useState(0);
 
     useEffect(() => {
@@ -25,13 +29,13 @@ function Index() {
                 <a href="#j" className="logo">AnyaTutors</a>
             </header>
             <section>
-                <img src="img_index/stars.png" alt="img" id="stars" style={{ left: scrollValue * 0.25 + 'px' }} type="image/png" />
-                <img src="img_index/mountains_behind.png" alt="img" id="mountains_behind" style={{ top: scrollValue * 0.5 + 'px' }} /> type="image/png"
-                <img src="img_index/Moon2.png" alt="img" id="moon" style={{ top: 100 + scrollValue * 1.5 + 'px' }} type="image/png" />
+                <img src={stars} alt="img" id="stars" style={{ left: scrollValue * 0.25 + 'px' }} type="image/png" />
+                <img src={mountainBack} alt="img" id="mountains_behind" style={{ top: scrollValue * 0.5 + 'px' }} type="image/png" />
+                <img src={luna} alt="img" id="moon" style={{ top: 100 + scrollValue * 1.5 + 'px' }} type="image/png" />
+                <img src={mountainFront} alt="img" id="mountains_front" style={{ top: scrollValue * 0 + 'px' }} type="image/png" />
                 <h2 id="text" style={{ marginLeft: scrollValue * 6 + 'px', marginTop: scrollValue * 1.5 + 'px' }}>Bienvenidos</h2>
-                <a href="/role" onClick={navigate('/role')} className="btn" id="bt" style={{ marginTop: scrollValue * 1.5 + 'px' }}>Registrarse</a>
-                <a href="/UserLogIn" onClick={navigate('/UserLogIn')} className="btn" alt="img" id="bt2" style={{ marginTop: scrollValue * 1.5 + 'px' }}>Iniciar sesion</a>
-                <img src="img_index/mountains_front.png" alt="img" id="mountains_front" style={{ top: scrollValue * 0 + 'px' }} type="image/png" />
+                <button onClick={() => navigate('/role')} className="btn" id="bt" style={{ marginTop: scrollValue * 1.5 + 'px' }}>Registrarse</button>
+                <button onClick={() => navigate('/UserLogIn')} className="btn" alt="img" id="bt2" style={{ marginTop: scrollValue * 1.5 + 'px' }}>Iniciar sesion</button>
 
             </section>
             <div className="sec" id="sec">
