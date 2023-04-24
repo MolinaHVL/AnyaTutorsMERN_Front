4c72a0423d6926e1c325033763ffbe144562ba51
MolinaHVL <christopher.molina@cetys.edu.mx>
2023-04-24 14:04:11 -0700

    improving css adminpage

--- anya_front/src/components/AllStudents.jsx
+++ anya_front/src/components/AllStudents.jsx
@@ -1,19 +1,28 @@
 import { Grid } from "@mui/material";
 import StudentInfo from "./StudentInfo";
+import Top from "./BodyMaestro/Top Section/Top";
 
 const AllStudents = ({ students }) => {
     return (
-        <Grid container spacing={2} sx={{ mt: 1 }}>
-            {students.map((student) => {
-                return (
-                    <Grid item xs={12} sm={6} md={4} key={student._id}>
-                        <StudentInfo
-                            student={student}
-                        />
-                    </Grid>
-                );
-            })}
-        </Grid>
+        <div className='mainContentAdmin'>
+            <Top />
+
+            <div className='bottom flex'>
+                <Grid container spacing={2} sx={{ mt: 1 }}>
+                    {students.map((student) => {
+                        return (
+                            <Grid item xs={12} sm={6} md={4} key={student._id}>
+                                <StudentInfo
+                                    student={student}
+                                />
+                            </Grid>
+                        );
+                    })}
+                </Grid>
+
+            </div>
+
+        </div>
     );
 }
 
--- anya_front/src/components/StudentInfo.jsx
+++ anya_front/src/components/StudentInfo.jsx
@@ -1,26 +1,47 @@
 import { Box, Stack, Typography } from "@mui/material";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
 
 const StudentInfo = ({ student }) => {
     return (
-        <Box sx={{ backgroundColor: "white", height: 480, border: 1 }}>
+        <Box
+            sx={{
+                backgroundColor: "white",
+                height: 380,
+                border: 1,
+                borderRadius: 10,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+            }}
+
+        >
             <Stack
-                sx={{ height: 0.6, px: 4, width: 0.75 }}
-                justifyContent="space-around"
+                sx={{
+                    height: 0.8,
+                    px: 4,
+                    width: 0.9,
+                    paddingTop: 0.1,
+                    center: true,
+                    justifyContent: "center"
+                }}
+                alignItems={"center"}
                 spacing={2}
             >
+                <AccountCircleIcon sx={{ fontSize: "80px" }} />
                 <Stack spacing={1} sx={{ width: 1 }}>
-                    <Typography fontSize={24}>{student.email}</Typography>
-                    <Typography fontSize={24}>{student.password}</Typography>
+                    <Typography fontSize={20} fontFamily={'Poppins'} sx={{ fontWeight: 'bold' }}>Email: </Typography>
+                    <Typography fontSize={16} fontFamily={'Poppins'} sx={{ overflowWrap: "break-word" }}>{student.email}</Typography>
+                    <Typography fontSize={20} fontFamily={'Poppins'} sx={{ fontWeight: 'bold' }}>Password: </Typography>
+                    <Typography fontSize={16} fontFamily={'Poppins'} sx={{ overflowWrap: "break-word" }}>{student.password}</Typography>
                 </Stack>
                 <Stack spacing={1} sx={{ width: 1 }}>
-                    <Typography fontSize={16}>Role:</Typography>
-                    <Typography fontSize={24}>Student</Typography>
+                    <Typography fontSize={16} fontFamily={'Poppins'} sx={{ fontWeight: 'bold' }}>Role:</Typography>
+                    <Typography fontSize={20} fontFamily={'Poppins'}>Student</Typography>
                 </Stack>
             </Stack>
         </Box>
-
-
     );
-}
+};
 
 export default StudentInfo;
