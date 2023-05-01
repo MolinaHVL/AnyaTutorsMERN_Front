c4f0627597f7a07e58f554126785c0e66da7bc57
MolinaHVL <christopher.molina@cetys.edu.mx>
2023-04-30 21:04:00 -0700

    Update ChatGlobal.jsx

--- anya_front/src/components/ChatGlobal.jsx
+++ anya_front/src/components/ChatGlobal.jsx
@@ -12,7 +12,7 @@ import {
 import { io } from "socket.io-client"
 import useUser from "../hooks/useUser";
 
-const socket = io('http://localhost:4000')
+const socket = io(process.env.REACT_APP_API_URL)
 
 const Chat = () => {
 
